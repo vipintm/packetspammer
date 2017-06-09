6be08b05ace24d85d7d10a298f3d63ef0adce312
Vipin Madhavanunni <vipintm@gmail.com>
2017-06-09 18:09:53 +0200

    Adding missing

--- using_filters.c
+++ using_filters.c
@@ -10,6 +10,72 @@
 #include <linux/ip.h>
 #include <linux/udp.h>
 
+/* Defined in include/linux/ieee80211.h */
+struct ieee80211_hdr {
+	uint16_t /*__le16*/frame_control;
+	uint16_t /*__le16*/duration_id;
+	uint8_t addr1[6];
+	uint8_t addr2[6];
+	uint8_t addr3[6];
+	uint16_t /*__le16*/seq_ctrl;
+//uint8_t addr4[6];
+}__attribute__ ((packed));
+
+static const uint8_t u8aRadiotapHeader[] = {
+
+0x00, 0x00, // <-- radiotap version (ignore this)
+		0x18, 0x00, // <-- number of bytes in our header (count the number of "0x"s)
+
+		/**
+		 * The next field is a bitmap of which options we are including.
+		 * The full list of which field is which option is in ieee80211_radiotap.h,
+		 * but I've chosen to include:
+		 *   0x00 0x01: timestamp
+		 *   0x00 0x02: flags
+		 *   0x00 0x03: rate
+		 *   0x00 0x04: channel
+		 *   0x80 0x00: tx flags (seems silly to have this AND flags, but oh well)
+		 */
+		0x0f, 0x80, 0x00, 0x00,
+
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // <-- timestamp
+
+		/**
+		 * This is the first set of flags, and we've set the bit corresponding to
+		 * IEEE80211_RADIOTAP_F_FCS, meaning we want the card to add a FCS at the end
+		 * of our buffer for us.
+		 */
+		0x10,
+
+		0x00, // <-- rate
+		0x00, 0x00, 0x00, 0x00, // <-- channel
+
+		/**
+		 * This is the second set of flags, specifically related to transmissions. The
+		 * bit we've set is IEEE80211_RADIOTAP_F_TX_NOACK, which means the card won't
+		 * wait for an ACK for this frame, and that it won't retry if it doesn't get
+		 * one.
+		 */
+		0x08, 0x00, };
+
+/**
+ * After an 802.11 MAC-layer header, a logical link control (LLC) header should
+ * be placed to tell the receiver what kind of data will follow (see IEEE 802.2
+ * for more information).
+ *
+ * For political reasons, IP wasn't allocated a global so-called SAP number,
+ * which means that a simple LLC header is not enough to indicate that an IP
+ * frame was sent. 802.2 does, however, allow EtherType types (the same kind of
+ * type numbers used in, you guessed it, Ethernet) through the use of the
+ * "Subnetwork Access Protocol", or SNAP. To use SNAP, the three bytes in the
+ * LLC have to be set to the magical numbers 0xAA 0xAA 0x03. The next five bytes
+ * are then interpreted as a SNAP header. To specify an EtherType, we need to
+ * set the first three of them to 0. The last two bytes can then finally be set
+ * to 0x0800, which is the IP EtherType.
+ */
+const uint8_t ipllc[8] = { 0xaa, 0xaa, 0x03, 0x00, 0x00, 0x00, 0x08, 0x00 };
+
+
 void print_packet_info(const u_char *packet, struct pcap_pkthdr packet_header);
 void my_packet_handler(u_char *args, const struct pcap_pkthdr *header,
 		const u_char *packet);
